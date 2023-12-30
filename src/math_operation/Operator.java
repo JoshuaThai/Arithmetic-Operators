657cae596fad78124a7405e3a13089b8550fd9c9
JoshuaThai <145885143+JoshuaThai@users.noreply.github.com>
2023-12-29 21:46:29 -0600

    Add files via upload

---
+++ src/math_operation/Operator.java
@@ -0,0 +1,15 @@
+package math_operation;
+
+public interface Operator {
+	public int add(int x, int y);
+	
+	public int subtract(int x, int y);
+	
+	public int multiply(int x, int y);
+	
+	public int divide(int x, int y);
+	
+	public int modulo(int x, int y);
+	
+	public int exponent(int x, int y);
+}
